0ed11dc3d26a2188081f282ac0674cea104baa4d
daameya <1998ameya@gmail.com>
2025-12-09 17:30:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Ameya",
+    author_email="1998ameya@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
